1055259d23fa9677d1e40cd682643f5cad91791e
Aaron Williamson <wikea1@gmail.com>
2014-04-14 23:07:47 -0700

    added filter preventions for listings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
 
 private
+  def require_login
+    unless current_user
+      flash[:notice] = 'You must be logged in'
+      redirect_to sessions_path
+    end
+  end
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
